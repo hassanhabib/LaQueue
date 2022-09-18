cc916c7bef79cc8c9bb93860a8d877873a30919b
Hassan Habib <hassanhabib@live.com>
2022-09-17 18:39:13 -0700

    INFRA: Add Infra Manual Tests

---
+++ LaQueue.Tests.Manual/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using LaQueue.Web.Clients;
+
+Console.WriteLine("Hello, World!");
+
+var apiServerClient = new ApiClient();
+apiServerClient.CreatePublisherEndpoint<Student>(HandlerOfThings, "students");
+apiServerClient.RunApiServer("https://localhost:1985");
+
+ValueTask HandlerOfThings(Student student)
+{
+    DoSomethingWithStudent(student);
+
+    return ValueTask.CompletedTask;
+} 
+
+static void DoSomethingWithStudent(Student student) =>
+    Console.WriteLine(student.Name);
+
+public class Student
+{
+    public string Name { get; set; }
+}
