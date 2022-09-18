47450f1ab9509dc26f5dcecf52ab55173632bac8
Hassan Habib <hassanhabib@live.com>
2022-09-17 20:06:09 -0700

    BROKERS: POST Api Routine

---
+++ LaQueue/Brokers/Apis/IApiBroker.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace LaQueue.Brokers.Apis
+{
+    public interface IApiBroker
+    {
+        ValueTask<T> PostAsync<T>(string url, T request);
+    }
+}
