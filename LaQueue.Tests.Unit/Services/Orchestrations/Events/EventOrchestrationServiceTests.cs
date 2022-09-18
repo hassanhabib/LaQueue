44aed5f7b258d2597259b753eaf59689067b6128
Hassan Habib <hassanhabib@live.com>
2022-09-17 20:34:37 -0700

    ShouldPublishEventAsync -> FAIL

---
+++ LaQueue.Tests.Unit/Services/Orchestrations/Events/EventOrchestrationServiceTests.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using LaQueue.Services.Foundations.EventPublishes;
+using LaQueue.Services.Foundations.EventSubscriptions;
+using LaQueue.Services.Orchestrations.Events;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace LaQueue.Tests.Unit.Services.Orchestrations.Events
+{
+    public partial class EventOrchestrationServiceTests
+    {
+        private readonly Mock<IEventPublishService> eventPublishServiceMock;
+        private readonly Mock<IEventSubscriptionService> eventSubscriptionServiceMock;
+        private readonly IEventOrchestrationService eventOrchestrationService;
+
+        public EventOrchestrationServiceTests()
+        {
+            this.eventPublishServiceMock = new Mock<IEventPublishService>();
+            this.eventSubscriptionServiceMock = new Mock<IEventSubscriptionService>();
+
+            this.eventOrchestrationService = new EventOrchestrationsService(
+                eventPublishService: this.eventPublishServiceMock.Object,
+                eventSubscriptionService: this.eventSubscriptionServiceMock.Object);
+        }
+
+        private static string GetRandomEventName() =>
+            new MnemonicString().GetValue();
+    }
+}
---
+++ LaQueue/Services/Orchestrations/Events/IEventOrchestrationService.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace LaQueue.Services.Orchestrations.Events
+{
+    public interface IEventOrchestrationService
+    {
+        ValueTask<T> PublishEventAsync<T>(T @event, string eventName);
+    }
+}
