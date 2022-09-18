cc1b97f3a099965d8d94c18dedd886439275a7bb
Hassan Habib <hassanhabib@live.com>
2022-09-17 19:35:24 -0700

    ShouldRegisterEventWithEventName -> FAIL

---
+++ LaQueue/Services/Foundations/EventSubscriptions/EventSubscriptionService.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using LaQueue.Brokers.ApiServers;
+
+namespace LaQueue.Services.Foundations.EventSubscriptions
+{
+    public class EventSubscriptionService : IEventSubscriptionService
+    {
+        private readonly IApiServerBroker apiServerBroker;
+
+        public EventSubscriptionService(IApiServerBroker apiServerBroker) =>
+            this.apiServerBroker = apiServerBroker;
+
+        public void RegisterEventHandler<T>(Func<T, ValueTask> eventHandler, string eventName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LaQueue/Services/Foundations/EventSubscriptions/IEventSubscriptionService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+
+namespace LaQueue.Services.Foundations.EventSubscriptions
+{
+    public interface IEventSubscriptionService
+    {
+        void RegisterEventHandler<T>(Func<T, ValueTask> eventHandler, string eventName);
+    }
+}
