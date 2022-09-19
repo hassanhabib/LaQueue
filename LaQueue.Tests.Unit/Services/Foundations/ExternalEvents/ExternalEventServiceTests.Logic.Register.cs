d25f9f6b7f1cdc683e5c410a34a8480b5672d174
Hassan Habib <hassanhabib@live.com>
2022-09-19 00:29:53 -0700

    ShouldRegisterEventHandler -> FAIL

---
+++ LaQueue.Tests.Unit/Services/Foundations/ExternalEvents/ExternalEventServiceTests.Logic.Register.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Moq;
+using Xunit;
+
+namespace LaQueue.Tests.Unit.Services.Foundations.ExternalEvents
+{
+    public partial class ExternalEventServiceTests
+    {
+        [Fact]
+        public void ShouldRegisterEventHandler()
+        {
+            // given
+            string randomEventName = GetRandomEventName();
+            string inputEventName = randomEventName;
+
+            var inputEventHandlerMock =
+                new Mock<Func<object, ValueTask>>();
+
+            Func<object, ValueTask> inputEventHandler =
+                inputEventHandlerMock.Object;
+
+            object randomObject = GetRandomObject();
+            object incomingObject = randomObject;
+            Message objectMessage = CreateObjectMessage(incomingObject);
+
+            this.queueBrokerMock.Setup(broker =>
+                broker.RegisterEventListener(
+                    It.IsAny<Func<Message, CancellationToken, Task>>(), inputEventName))
+                        .Callback<Func<Message, CancellationToken, Task>, string>((eventFunction, eventName) =>
+                            eventFunction.Invoke(objectMessage, It.IsAny<CancellationToken>()));
+
+            // when
+            this.externalEventService.RegisterEventHandler(
+                eventHandler: inputEventHandlerMock.Object,
+                eventName: inputEventName);
+
+            // then
+            inputEventHandlerMock.Verify(handler =>
+                handler.Invoke(It.Is(
+                    SameObjectEventAs(incomingObject))),
+                        Times.Once);
+
+            this.queueBrokerMock.Verify(broker =>
+                broker.RegisterEventListener(
+                    It.IsAny<Func<Message, CancellationToken, Task>>(), inputEventName),
+                        Times.Once);
+
+            this.queueBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ LaQueue.Tests.Unit/Services/Foundations/ExternalEvents/ExternalEventServiceTests.cs
@@ -0,0 +1,62 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Linq.Expressions;
+using System;
+using System.Text;
+using KellermanSoftware.CompareNetObjects;
+using LaQueue.Brokers.Queues;
+using LaQueue.Services.Foundations.ExternalEvents;
+using Microsoft.Azure.ServiceBus;
+using Moq;
+using Newtonsoft.Json;
+using Tynamix.ObjectFiller;
+
+namespace LaQueue.Tests.Unit.Services.Foundations.ExternalEvents
+{
+    public partial class ExternalEventServiceTests
+    {
+        private readonly Mock<IQueueBroker> queueBrokerMock;
+        private readonly ICompareLogic compareLogic;
+        private readonly IExternalEventService externalEventService;
+
+        public ExternalEventServiceTests()
+        {
+            this.queueBrokerMock = new Mock<IQueueBroker>();
+            this.compareLogic = new CompareLogic();
+
+            this.externalEventService = new ExternalEventService(
+                queueBroker: this.queueBrokerMock.Object);
+        }
+
+        private static string GetRandomEventName() =>
+            new MnemonicString().GetValue();        
+        
+        private static string GetRandomObject() =>
+            new MnemonicString().GetValue();
+
+        private Expression<Func<object, bool>> SameObjectEventAs(
+            object expectedObject)
+        {
+            return actualContributionEvent =>
+                this.compareLogic.Compare(
+                    expectedObject,
+                    actualContributionEvent)
+                        .AreEqual;
+        }
+
+        private static Message CreateObjectMessage(object @object)
+        {
+            string serializedStudent = JsonConvert.SerializeObject(@object);
+            byte[] contributionBody = Encoding.UTF8.GetBytes(serializedStudent);
+
+            return new Message
+            {
+                Body = contributionBody
+            };
+        }
+    }
+}
