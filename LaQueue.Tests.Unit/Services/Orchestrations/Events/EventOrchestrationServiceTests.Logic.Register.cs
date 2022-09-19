baf838f047b608dccba0fdd035c04612d8e67f22
Hassan Habib <hassanhabib@live.com>
2022-09-19 01:06:17 -0700

    ShouldSubscribeEventHandlerWithExternalEvent -> FAIL

--- LaQueue.Tests.Unit/Services/Orchestrations/Events/EventOrchestrationServiceTests.Logic.Register.cs
+++ LaQueue.Tests.Unit/Services/Orchestrations/Events/EventOrchestrationServiceTests.Logic.Register.cs
@@ -57,5 +57,49 @@ namespace LaQueue.Tests.Unit.Services.Orchestrations.Events
             this.externalEventServiceMock.VerifyNoOtherCalls();
             this.eventPublishServiceMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public void ShouldSubscribeEventHandlerWithExternalEvent()
+        {
+            // given
+            string localConnectionString = "something.servicebus.com";
+
+            var eventHandlerMock =
+                new Mock<Func<object, ValueTask>>();
+
+            Func<object, ValueTask> eventHandler =
+                eventHandlerMock.Object;
+
+            string randomEventName = GetRandomEventName();
+            string inputEventName = randomEventName;
+
+            this.eventOrchestrationService = new EventOrchestrationService(
+                connectionString: localConnectionString,
+                eventPublishService: this.eventPublishServiceMock.Object,
+                eventSubscriptionService: this.eventSubscriptionServiceMock.Object,
+                externalEventService: this.externalEventServiceMock.Object);
+
+            // when
+            this.eventOrchestrationService.SubscribeEventHandler(
+                eventHandler,
+                inputEventName);
+
+            // then
+            this.externalEventServiceMock.Verify(service =>
+                service.RegisterEventHandler(
+                    eventHandler,
+                    inputEventName),
+                        Times.Once);
+
+            this.eventSubscriptionServiceMock.Verify(service =>
+                service.RegisterEventHandler(
+                    eventHandler,
+                    inputEventName),
+                        Times.Never);
+
+            this.eventSubscriptionServiceMock.VerifyNoOtherCalls();
+            this.externalEventServiceMock.VerifyNoOtherCalls();
+            this.eventPublishServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
