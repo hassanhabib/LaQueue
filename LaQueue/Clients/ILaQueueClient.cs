973caabea82bbe3390d407fb731bcea5e2a55ede
Hassan Habib <hassanhabib@live.com>
2022-09-19 01:19:05 -0700

    CLIENTS: LaQueue Client Functions

---
+++ LaQueue/Clients/ILaQueueClient.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using System;
+
+namespace LaQueue.Clients
+{
+    public interface ILaQueueClient
+    {
+        ValueTask<T> PublishEventAsync<T>(T @event, string eventName);
+        void SubscribeEventHandler<T>(Func<T, ValueTask> eventHandler, string eventName);
+    }
+}
